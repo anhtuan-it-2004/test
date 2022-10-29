c77cbe3b2c96879c39e4db8ea5e7ea8661fd47c3
anhtuan-it-2004 <anhtuan2004.work@gmail.com>
2022-10-29 23:04:07 +0700

    Sua file

--- bai81.c
+++ bai81.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
+    #include<stdio.h>
 
-double Circumference(double r){
-    return 2 * 3.14 *r;
-}
+    double Circumference(double r){
+        return 2 * 3.14 *r;
+    }
 
-int main(){
-    double r;
-    scanf("%lf", &r);
-    printf("%f", Circumference(r));
+    int main(){
+        double r1, r2;
+        scanf("%lf", &r1);
+        printf("%f\n", Circumference(r1));
+        scanf("%lf", &r2);
+        printf("%f", Circumference(r2));
 
-    return 0;
-}
+        return 0;
+    }
