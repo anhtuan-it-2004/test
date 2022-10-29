f32322f26e032c9796871250102c4252053b545b
anhtuan-it-2004 <anhtuan2004.work@gmail.com>
2022-10-29 22:45:59 +0700

    Add bai80 file

--- test.c
+++ test.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 
+int countOddNumberInArray(int arr[], int n){
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] % 2 == 1){
+            count++;
+        }
+    }
+    return count;
+}
+
 int main(){
-    printf("Hello World!");
-    return 0;
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("%d", countOddNumberInArray(arr, n));
 }
